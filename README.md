61a5f537f8e2a9fcfec86be8f6ca8abb09470771
Adrian Frühwirth <bonki@users.noreply.github.com>
2018-11-29 23:58:49 +0100

    Initial commit, v1.1.10.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# chocolatey-wina1314-driver
+
+Chocolatey package for [WinA1314](https://github.com/jsakamoto/WinA1314).
+
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$url   = 'https://github.com/bonki/WinA1314/releases/download/v1.1.10.0/WinA1314-setup-x86.exe'
+$url64 = 'https://github.com/bonki/WinA1314/releases/download/v1.1.10.0/WinA1314-setup-x64.exe'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = $url
+  url64bit       = $url64
+  softwareName   = 'WinA1314*'
+  checksum       = 'a35af78833627684c6a9f760c15de2889f04c8a982873b73ab53af7c22430de1'
+  checksumType   = 'sha256'
+  checksum64     = 'a2bc4ef2a15427665c55a76cdedfa8cf71f7f22629371042ac0c5e1e6b5a9ed2'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -gt 1) {
+	Write-Warning "$($key.Count) matches found!"
+	Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+	Write-Warning "Please alert package maintainer the following keys were matched:"
+	$key | % {Write-Warning "- $($_.DisplayName)"}
+	return
+} elseif ($key.Count -eq 1) {
+	$key | % { 
+		$packageArgs['file'] = "$($_.UninstallString)"
+		Uninstall-ChocolateyPackage @packageArgs
+	}
+}
+
+Install-ChocolateyPackage @packageArgs
+
